f0a10bccc8fda1ce80577ae1195524c8bd1f536c
Aria Bjørnbakken Eide <Aria.Eide@student.uib.no>
2025-08-20 12:29:58 +0200

    3.5.12 CompareISODate

--- Initial.v
+++ Initial.v
@@ -1,4 +1,4 @@
-Require Import Coq.Numbers.BinNums.
+Require Import Coq.Numbers.BinNums ZArith.
 
 Record ISODateRecord : Type :=
   mkISODateRecord {
@@ -6,3 +6,12 @@ Record ISODateRecord : Type :=
     month : Z;
     day : Z;
   }.
+
+Definition CompareISODate (isoDate1 isoDate2 : ISODateRecord) : Z :=
+  if Z.gtb (year isoDate1) (year isoDate2) then 1
+  else if Z.ltb (year isoDate1) (year isoDate2) then -1
+  else if Z.gtb (month isoDate1) (month isoDate2) then 1
+  else if Z.ltb (month isoDate1) (month isoDate2) then -1
+  else if Z.gtb (day isoDate1) (day isoDate2) then 1
+  else if Z.ltb (day isoDate1) (day isoDate2) then -1
+  else 0.
