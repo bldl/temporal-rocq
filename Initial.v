54c691b7be5be2cfc4b285d97a65b780ecef6712
Aria Bjørnbakken Eide <Aria.Eide@student.uib.no>
2025-08-27 13:13:12 +0200

    msPerDay and more

--- Initial.v
+++ Initial.v
@@ -1,4 +1,6 @@
 Require Import Coq.Numbers.BinNums ZArith.
+Open Scope bool_scope.
+Open Scope Z.
 
 (* 3.5.1 ISODateRecord *)
 Record ISODateRecord : Type :=
@@ -21,7 +23,7 @@ Definition CompareISODate (isoDate1 isoDate2 : ISODateRecord) : Z :=
 Theorem CompareISODate_eq_implies_eq_zero :
   forall (y1 y2 m1 m2 d1 d2 : Z),
   y1 = y2 /\ m1 = m2 /\ d1 = d2 ->
-  CompareISODate (mkISODateRecord y1 m1 d1) (mkISODateRecord y2 m2 d2) = 0%Z.
+  CompareISODate (mkISODateRecord y1 m1 d1) (mkISODateRecord y2 m2 d2) = 0.
 Proof.
   intros y1 y2 m1 m2 d1 d2.
   intro H.
@@ -40,13 +42,70 @@ Proof.
   reflexivity.
 Qed.
 
+(* TODO assumptions assummptions *)
+Definition msPerDay : Z := 86400000.
+
+Definition EpochDayNumberForYear (y : Z) : Z :=
+  365 * (y - 1970) + ((y - 1969) / 4) - ((y - 1901) / 100) + ((y - 1601) / 400).
+
+Definition EpochTimeForYear (y : Z) : Z :=
+  msPerDay * EpochDayNumberForYear y.
+
+Definition EpochTimeToEpochYear (t : Z) : Z := 0.
+
+Definition MathematicalDaysInYear (y : Z) : Z :=
+  match (y mod 4) =? 0, (y mod 100) =? 0, (y mod 400) =? 0 with
+  | false, _,     _    => 365
+  | true,  false, _    => 366
+  | _,     true, false => 365
+  | _,     _,    true  => 366
+  end.
+
+Lemma MathematicalDaysInYear_365_or_366 :
+  forall (y : Z),
+  MathematicalDaysInYear y = 365 \/ MathematicalDaysInYear y = 366.
+Proof.
+  intro y.
+  unfold MathematicalDaysInYear.
+  destruct (y mod 4 =? 0).
+  destruct (y mod 100 =? 0).
+  destruct (y mod 400 =? 0).
+  right.
+  reflexivity.
+  left.
+  reflexivity.
+  right.
+  reflexivity.
+  left.
+  reflexivity.
+Qed.
+
+Lemma MathematicalDaysInYear_help :
+  forall (y : Z)
+  (ne_365 : MathematicalDaysInYear y <> 365)
+  (ne_366 : MathematicalDaysInYear y <> 366),
+  False.
+Proof.
+  intros y ne_365 ne_366.
+  destruct (MathematicalDaysInYear_365_or_366 y).
+  contradiction.
+  contradiction.
+Qed.
+
+Definition MathematicalInLeapYear (t : Z) : Z :=
+  match MathematicalDaysInYear (EpochTimeToEpochYear t) with
+  | 365 => 0
+  | 366 => 1
+  | _ => False_rect Z (MathematicalDaysInYear_help (EpochTimeToEpochYear t))
+  end.
+
 (* TODO: Assert month is 2 and add leap day *)
 (* 12.3.17 ISODaysInMonth *)
 Definition ISODaysInMonth (year month : Z) : Z :=
   match month with
-  | 1%Z | 3%Z | 5%Z | 7%Z | 8%Z | 10%Z | 12%Z => 31%Z
-  | 4%Z | 6%Z | 9%Z | 11%Z => 30%Z
-  | _ => 28%Z
+  | 1 | 3 | 5 | 7 | 8 | 10 | 12 => 31
+  | 4 | 6 | 9 | 11 => 30
+  | _ => 28
   end.
 
 (* 3.5.7 IsValidISODate *)
