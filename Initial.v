5b3331d6e517a7ecf9842ec82c267a7eb2b5089a
Aria Bjørnbakken Eide <Aria.Eide@student.uib.no>
2025-09-04 14:09:19 +0200

    Defining assertion notations

--- Initial.v
+++ Initial.v
@@ -1,7 +1,11 @@
 Require Import Coq.Numbers.BinNums ZArith.
-Require Import Coq.Program.Wf.
+Require Import Coq.Program.Wf Lia.
 Open Scope bool_scope.
 Open Scope Z.
+
+Definition assert (P : Prop) (proof : P) : unit := tt.
+Notation "'assert' P 'in' A" := (let tt := assert P _ in A) (at level 200).
+Notation "'impossible'" := (False_rect _ _).
 
 (* 3.5.1 ISODateRecord *)
 (*>>
@@ -204,17 +208,41 @@ Definition MathematicalInLeapYear (t : Z) : Z :=
 (* 13.3 end *)
 
 (* 12.3.17 ISODaysInMonth *)
-Definition ISODaysInMonth (year month : Z) : Z :=
+Program Definition ISODaysInMonth (year month : Z) (h : 1 <= month <= 12) : Z :=
   match month with
   (*>> 1. If month is 1, 3, 5, 7, 8, 10, or 12, return 31. <<*)
   | 1 | 3 | 5 | 7 | 8 | 10 | 12 => 31
   (*>> 2. If month is 4, 6, 9, or 11, return 30. <<*)
   | 4 | 6 | 9 | 11 => 30
-  (*>> 3. Assert: month = 2. <<*)
-  (* TODO *)
-  (*>> 4. Return 28 + MathematicalInLeapYear(EpochTimeForYear(year)). <<*)
-  | _ => 28 + MathematicalInLeapYear (EpochTimeForYear year)
+  | month =>
+      (*>> 3. Assert: month = 2. <<*)
+      assert month = 2 in
+      (*>> 4. Return 28 + MathematicalInLeapYear(EpochTimeForYear(year)). <<*)
+      28 + MathematicalInLeapYear (EpochTimeForYear year)
   end.
+
+(* assert month = 2 *)
+Next Obligation.
+Proof.
+  lia.
+Qed.
+
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
+Next Obligation. Proof. easy. Qed.
 
 (* 3.5.7 IsValidISODate *)
 Definition IsValidISODate (year month day : Z) : bool :=
