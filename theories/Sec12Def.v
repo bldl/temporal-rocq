3ab4a362edd90994abf0654b427593a294e6b3af
Aria Bjørnbakken Eide <Aria.Eide@student.uib.no>
2025-09-10 13:04:42 +0200

    Use numbers in section 13 MathematicalDaysInYear

--- theories/Sec12Def.v
+++ theories/Sec12Def.v
@@ -1,9 +1,13 @@
-Require Import Coq.Numbers.BinNums Coq.Program.Wf ZArith.
+Require Import Coq.Numbers.BinNums Coq.Program.Wf ZArith Lia.
 From Temporal Require Import Basic Sec13Def.
 Open Scope bool_scope.
 Open Scope Z.
 
 (* 12.3.17 ISODaysInMonth *)
+(*>> The abstract operation ISODaysInMonth takes arguments year (an integer) and
+     month (an integer in the inclusive interval from 1 to 12) and returns a
+     positive integer. It returns the number of days in the given year and month
+     in the ISO 8601 calendar. It performs the following steps when called: <<*)
 Program Definition ISODaysInMonth (year month : Z) (h : 1 <= month <= 12) : Z :=
   match month with
   (*>> 1. If month is 1, 3, 5, 7, 8, 10, or 12, return 31. <<*)
