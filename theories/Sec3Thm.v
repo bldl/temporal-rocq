a93b75ef564c332e63b57353d143122915fafa3d
Aria Bjørnbakken Eide <Aria.Eide@student.uib.no>
2025-09-10 17:52:13 +0200

    Fix theorem CompareISODate_eq_implies_eq_zero

--- theories/Sec3Thm.v
+++ theories/Sec3Thm.v
@@ -3,20 +3,23 @@ From Temporal Require Export Sec3Def.
 Open Scope bool_scope.
 Open Scope Z.
 
-
 Theorem CompareISODate_eq_implies_eq_zero :
   forall (y1 y2 m1 m2 d1 d2 : Z),
   y1 = y2 /\ m1 = m2 /\ d1 = d2 ->
-  CompareISODate (mkISODateRecord y1 m1 d1) (mkISODateRecord y2 m2 d2) = 0.
+  forall (m1_valid : 1 <= m1 <= 12) (m2_valid : 1 <= m2 <= 12)
+  (d1_valid : 1 <= d1 <= 31) (d2_valid : 1 <= d2 <= 31),
+  CompareISODate (mkISODateRecord y1 m1 m1_valid d1 d1_valid) (mkISODateRecord y2 m2 m2_valid d2 d2_valid) = 0.
 Proof.
   intros y1 y2 m1 m2 d1 d2.
   intro H.
+  intros m1_valid m2_valid d1_valid d2_valid.
   destruct H.
   destruct H0.
+  unfold CompareISODate.
+  simpl.
   rewrite H.
   rewrite H0.
   rewrite H1.
-  unfold CompareISODate.
   rewrite Z.gtb_ltb.
   rewrite Z.gtb_ltb.
   rewrite Z.gtb_ltb.
