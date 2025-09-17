72cd9f030b8684b79551150a91a278f8dd182f11
Aria Bjørnbakken Eide <Aria.Eide@student.uib.no>
2025-09-17 13:58:29 +0200

    Move theorem `TimeRecord_IsValidTime`

--- theories/Sec4Thm.v
+++ theories/Sec4Thm.v
@@ -2,6 +2,38 @@ Require Import ZArith.
 From Temporal Require Import Sec4Def.
 Open Scope Z.
 
+Theorem TimeRecord_IsValidTime :
+  forall (t : TimeRecord),
+  IsValidTime (hour t) (minute t) (second t) (millisecond t) (microsecond t) (nanosecond t) = true.
+Proof.
+  intro t.
+  destruct t.
+  simpl.
+  unfold IsValidTime.
+
+  1: destruct_with_eqn ((hour <? 0) || (hour >? 23)).
+  2: destruct_with_eqn ((minute <? 0) || (minute >? 59)).
+  3: destruct_with_eqn ((second <? 0) || (second >? 59)).
+  4: destruct_with_eqn ((millisecond <? 0) || (millisecond >? 999)).
+  5: destruct_with_eqn ((microsecond <? 0) || (microsecond >? 999)).
+  6: destruct_with_eqn ((nanosecond <? 0) || (nanosecond >? 999)).
+
+  - exfalso.
+    exact (inside_range_outside_range_impossible' hour_valid Heqb).
+  - exfalso.
+    exact (inside_range_outside_range_impossible' minute_valid Heqb0).
+  - exfalso.
+    exact (inside_range_outside_range_impossible' second_valid Heqb1).
+  - exfalso.
+    exact (inside_range_outside_range_impossible' millisecond_valid Heqb2).
+  - exfalso.
+    exact (inside_range_outside_range_impossible' microsecond_valid Heqb3).
+  - exfalso.
+    exact (inside_range_outside_range_impossible' nanosecond_valid Heqb4).
+
+  - reflexivity.
+Qed.
+
 (* Proofs that BalanceTime is missing a precondition *)
 Theorem delta_days_can_be_negative : exists hour, days (BalanceTime hour 0 0 0 0 0) < 0.
 Proof.
