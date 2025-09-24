6ef2c9fb7ae40ad283136044c11f1398e8e216c2
Vebjørn Øiestad <vebjorn.oiestad@gmail.com>
2025-09-24 12:29:43 +0200

    Add 5.5.3 CombineISODateAndTimeRecord

--- theories/Sec5Def.v
+++ theories/Sec5Def.v
@@ -1,4 +1,4 @@
-From Temporal Require Import Sec4Def Sec3Def.
+From Temporal Require Import Sec4Def Sec4Thm Sec3Def.
 
 (* 5.5.1 ISO Date-Time Records *)
 (*>> For any ISO Date-Time Record r,
@@ -15,3 +15,13 @@ mkISODateTimeRecord {
   Time : TimeRecord;
   Time_valid : IsValidTime (hour Time) (minute Time) (second Time) (millisecond Time) (microsecond Time) (nanosecond Time) = true;
   }.
+
+(*>> 5.5.3 CombineISODateAndTimeRecord <<*)
+Program Definition CombineISODateAndTimeRecord (isoDate : ISODateRecord) (time : TimeRecord) : ISODateTimeRecord :=
+  (*>> 1. NOTE: time.[[Days]] is ignored. <<*)
+  (*>> 2. Return ISO Date-Time Record { [[ISODate]]: isoDate, [[Time]]: time }. <<*)
+  mkISODateTimeRecord isoDate _ time _.
+
+(* Admitted until ISODateRecord actually is valid. *)
+Next Obligation. Admitted.
+Next Obligation. Proof. exact (TimeRecord_IsValidTime time). Qed.
