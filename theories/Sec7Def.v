dae9bd6ae2b6e0aab383d6fd2aa05047125d8166
Aria Bjørnbakken Eide <Aria.Eide@student.uib.no>
2025-09-24 13:56:13 +0200

    Define 7.5.1 `DateDurationRecord`

---
+++ theories/Sec7Def.v
@@ -0,0 +1,22 @@
+Require Import ZArith ZArith.Zpow_alt.
+Open Scope Z.
+
+Record Float64RepresentableInteger :=
+  mkFloat64RepresentableInteger {
+    value : Z;
+    in_range : -(2 ^^ 53) <= value <= (2 ^^ 53);
+  }.
+
+(* 7.5.1 Date Duration Records *)
+Record DateDurationRecord :=
+  mkDateDurationRecord {
+    (*>> Field Name | Value                           | Meaning <<*)
+    (*>> [[Years]]  | a float64-representable integer | The number of years in the duration. <<*)
+    years : Float64RepresentableInteger;
+    (*>> [[Months]] | a float64-representable integer | The number of months in the duration. <<*)
+    months : Float64RepresentableInteger;
+    (*>> [[Weeks]]  | a float64-representable integer | The number of weeks in the duration. <<*)
+    weeks : Float64RepresentableInteger;
+    (*>> [[Days]]   | a float64-representable integer | The number of days in the duration. <<*)
+    days : Float64RepresentableInteger;
+  }.
