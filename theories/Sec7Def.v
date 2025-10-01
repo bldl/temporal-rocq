9e55db4290d5317613fc413e59ac5bc1644d1ff9
Vebjørn Øiestad <102312685+voiestad@users.noreply.github.com>
2025-10-01 13:20:25 +0200

    Formatting

--- theories/Sec7Def.v
+++ theories/Sec7Def.v
@@ -23,18 +23,19 @@ Record DateDurationRecord :=
 
   (* 7.5.14 DateDurationSign *)
 Definition DateDurationSign (dateDuration : DateDurationRecord) : Z :=
-  let ValueSign := fix ValueSign (vs : list (DateDurationRecord -> Float64RepresentableInteger)) (dateDuration : DateDurationRecord): option Z :=
-  match vs with
-  | nil => None
-  | f :: vs' => let v := value (f dateDuration) in
+  let ValueSign := fix ValueSign (vs : list (DateDurationRecord -> Float64RepresentableInteger)) (dateDuration : DateDurationRecord) : option Z :=
+    match vs with
+    | nil => None
+    | f :: vs' => let v := value (f dateDuration) in
 
-    (*>> a. If v < 0, return -1. <<*)
-    if v <? 0 then Some (-1)
-    (*>> b. If v > 0, return 1. <<*)
-    else if v >? 0 then Some 1
+      (*>> a. If v < 0, return -1. <<*)
+      if v <? 0 then Some (-1)
+      (*>> b. If v > 0, return 1. <<*)
+      else if v >? 0 then Some 1
 
-    else ValueSign vs' dateDuration
-  end in
+      else ValueSign vs' dateDuration
+    end 
+  in
   (*>> 1. For each value v of « dateDuration.[[Years]], dateDuration.[[Months]], dateDuration.[[Weeks]], dateDuration.[[Days]] », do <<*)
   match ValueSign (years :: months :: weeks :: days :: nil) dateDuration with
   | Some x => x
