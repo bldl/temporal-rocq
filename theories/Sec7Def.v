eeff0b932cbd8a650ba06d6cf8ca0ba461a95ba2
Vebjørn Øiestad <102312685+voiestad@users.noreply.github.com>
2025-10-01 13:15:09 +0200

    Add 7.5.14 DateDurationSign

--- theories/Sec7Def.v
+++ theories/Sec7Def.v
@@ -1,4 +1,4 @@
-From Stdlib Require Import ZArith ZArith.Zpow_alt.
+From Stdlib Require Import ZArith ZArith.Zpow_alt List.
 Open Scope Z.
 
 Record Float64RepresentableInteger :=
@@ -20,3 +20,24 @@ Record DateDurationRecord :=
     (*>> [[Days]]   | a float64-representable integer | The number of days in the duration. <<*)
     days : Float64RepresentableInteger;
   }.
+
+  (* 7.5.14 DateDurationSign *)
+Definition DateDurationSign (dateDuration : DateDurationRecord) : Z :=
+  let ValueSign := fix ValueSign (vs : list (DateDurationRecord -> Float64RepresentableInteger)) (dateDuration : DateDurationRecord): option Z :=
+  match vs with
+  | nil => None
+  | f :: vs' => let v := value (f dateDuration) in
+
+    (*>> a. If v < 0, return -1. <<*)
+    if v <? 0 then Some (-1)
+    (*>> b. If v > 0, return 1. <<*)
+    else if v >? 0 then Some 1
+
+    else ValueSign vs' dateDuration
+  end in
+  (*>> 1. For each value v of « dateDuration.[[Years]], dateDuration.[[Months]], dateDuration.[[Weeks]], dateDuration.[[Days]] », do <<*)
+  match ValueSign (years :: months :: weeks :: days :: nil) dateDuration with
+  | Some x => x
+  (*>> 2. Return 0. <<*)
+  | None => 0
+  end.
