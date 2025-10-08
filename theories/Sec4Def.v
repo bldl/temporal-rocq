da1f009b3f3397a00b17ed58787d87c4d24ae6a0
Vebjørn Øiestad <102312685+voiestad@users.noreply.github.com>
2025-10-08 22:30:50 +0200

    Add 4.5.15 AddTime

--- theories/Sec4Def.v
+++ theories/Sec4Def.v
@@ -222,3 +222,12 @@ Next Obligation.
     destruct (nanosecond_valid time).
     lia.
 Qed.
+
+Definition MinTimeDuration : Z := (-9007199254740991999999999).
+Definition MaxTimeDuration : Z := 9007199254740991999999999.
+
+(* 4.5.15 AddTime *)
+Definition AddTime (time : TimeRecord) (timeDuration : Z) (timeDuration_valid : MinTimeDuration <= timeDuration <= MaxTimeDuration) : TimeRecord :=
+  (*>> 1. Return BalanceTime(time.[[Hour]], time.[[Minute]], time.[[Second]], time.[[Millisecond]], time.[[Microsecond]], time.[[Nanosecond]] + timeDuration). <<*)
+  BalanceTime (hour time) (minute time) (second time) (millisecond time) (microsecond time) (nanosecond time + timeDuration).
+  (*>> 2. NOTE: If using floating points to implement this operation, add the time components separately before balancing to avoid errors with unsafe integers. <<*)
