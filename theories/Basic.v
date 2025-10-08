3a5a7c1355d896155152e4ec2eb919080a65165b
Aria Bjørnbakken Eide <Aria.Eide@student.uib.no>
2025-10-08 12:20:05 +0200

    Define completion and exception types

--- theories/Basic.v
+++ theories/Basic.v
@@ -7,6 +7,12 @@ Notation "'assert' P 'in' A" := (let tt := assert P _ in A) (at level 200).
 Notation "'impossible'" := (False_rect _ _).
 Notation "a '!=?' b" := (negb (a =? b)) (at level 70).
 
+Inductive exception : Type := RangeError.
+
+Inductive completion (result : Type) : Type :=
+| Normal (_ : result)
+| Throw (_ : exception).
+
 Lemma eq_sym_iff {A} (x y : A) : x = y <-> y = x.
 Proof.
   split.
