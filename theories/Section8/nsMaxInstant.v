865a78cc9253f9a6f02a7bd220751a210a5a3ced
Vebjørn Øiestad <102312685+voiestad@users.noreply.github.com>
2025-10-15 13:37:09 +0200

    Fix typo in nsMaxInstant and nsMinInstant

--- theories/Section8/nsMaxInstant.v
+++ theories/Section8/nsMaxInstant.v
@@ -2,4 +2,4 @@ From Stdlib Require Import ZArith.
 From Temporal Require Import Section8.nsPerDay.
 Open Scope Z.
 
-Definition nsMaxInstant : Z := -(100000000 * nsPerDay).
+Definition nsMaxInstant : Z := 100000000 * nsPerDay.
