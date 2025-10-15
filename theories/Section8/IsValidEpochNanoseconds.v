9396726635c41bc39cc555afe0f0d7b9b788fd26
Vebjørn Øiestad <102312685+voiestad@users.noreply.github.com>
2025-10-15 13:35:15 +0200

    Fix typo in 8.5.1

--- theories/Section8/IsValidEpochNanoseconds.v
+++ theories/Section8/IsValidEpochNanoseconds.v
@@ -8,7 +8,7 @@ Open Scope Z.
 (* 8.5.1 IsValidEpochNanoseconds *)
 Definition IsValidEpochNanoseconds (epochNanoseconds : Z) : bool :=
   (*>> 1. If ℝ(epochNanoseconds) < nsMinInstant or ℝ(epochNanoseconds) > nsMaxInstant, then <<*)
-  if (epochNanoseconds <? nsMinInstant) || (epochNanoseconds <? nsMaxInstant)
+  if (epochNanoseconds <? nsMinInstant) || (epochNanoseconds >? nsMaxInstant)
     (*>> a. Return false. <<*)
     then false
   (*>> 2. Return true. <<*)
